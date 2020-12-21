1cd96842357f3e47acb49b1e402e9a72e83f1dc5
Ubuntu <ubuntu@ip-172-31-14-246.ec2.internal>
2020-12-21 03:57:20 +0000

    loop.cpp

---
+++ loop3.cpp
@@ -0,0 +1,43 @@
+// Copyright (c) 2020 Ahmad El-khawaldeh All rights reserved.
+//
+// Created by: Ahmad El-khawaldeh
+// Created on: Dec 2020
+// This program asks the user for a positive integer,
+// then multiplies all the whole numbers up to that number
+
+#include <iostream>
+#include <cmath>
+
+
+
+int main() {
+    int positiveInteger;
+    int loopCounter = 0;
+    int positive_exponent;
+    std::string positive_as_string;
+
+    std::cout << " I will give you the factroial of the number you input"
+    << std::endl;
+
+
+    // input
+    std::cout << "Enter a number : ";
+    std::cin >> positive_as_string;
+
+    // process
+     try {
+        positiveInteger = std::stoi(positive_as_string);
+        if (positiveInteger < 0) {
+            throw std::invalid_argument("");
+        }
+
+        for (loopCounter = 0; loopCounter
+            < positiveInteger + 1; loopCounter++) {
+            positive_exponent = pow(loopCounter, 2);
+            std::cout << loopCounter << "² =  " << positive_exponent
+            << std::endl;
+        }
+    }catch (std::invalid_argument) {
+            std::cout << "That was not a invalid integer" << std::endl;
+    }
+}
